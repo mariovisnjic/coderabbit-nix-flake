5475231ea03848c93ece8c838992f7449390f7dc
mariovisnjic <mario.visnjic@walls.io>
2026-05-06 12:05:36 +0200

    Allow unfree in flake's nixpkgs so the package builds standalone

--- flake.nix
+++ flake.nix
@@ -9,7 +9,10 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
 
         version = "0.4.5";
 
