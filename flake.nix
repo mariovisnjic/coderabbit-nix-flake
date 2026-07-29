27a8550a99a3140587fd3b7b51fd71b7323f7883
mariovisnjic <38807998+mariovisnjic@users.noreply.github.com>
2026-07-29 08:38:39 +0000

    Bump CodeRabbit to 0.7.1

--- flake.nix
+++ flake.nix
@@ -14,24 +14,24 @@
           config.allowUnfree = true;
         };
 
-        version = "0.7.0";
+        version = "0.7.1";
 
         sources = {
           "x86_64-linux" = {
             platform = "linux-x64";
-            hash = "sha256-o3A45u+lzFkTr2rO1VClgtnJzbkbSgSG+dNQ7wT6qqw=";
+            hash = "sha256-roOgYXb4TOfUfcJe+KysFXc51WqMpZ6n0QlPj6MMU3o=";
           };
           "aarch64-linux" = {
             platform = "linux-arm64";
-            hash = "sha256-Pj3vFdp/F9YB/5Dj0dge2TtJicaudQngWSbWBmmBWNQ=";
+            hash = "sha256-c4LMbY+Mlvy5PygQYm9hv21su57oEzRHg1CuvAQRvSo=";
           };
           "x86_64-darwin" = {
             platform = "darwin-x64";
-            hash = "sha256-qgpH/wGqhJ1h7tJ0ZPTuI2Afat1QwBRQwWiaWAKXDHY=";
+            hash = "sha256-WrBqZpKinIZUzzZZ2NlDzCt/c3ycMe7rsxhjiYeLJ78=";
           };
           "aarch64-darwin" = {
             platform = "darwin-arm64";
-            hash = "sha256-VTmRD9ZHRTUbRB0zCadn4WtJphkWzFl4PQm3SOKkgz4=";
+            hash = "sha256-kTbJa2+4mYuhGd+XWbTaKYckYQgmDFodnuhr2t9pRmA=";
           };
         };
 
